1643d3522d56ae386295e94d50fcefddfbda430f
Erick Carlos Sanchez Perez <ercsanchez@compartamos.com>
2017-03-09 17:24:59 -0600

    Added new structure

---
+++ DataManager.java
@@ -0,0 +1,135 @@
+package com.appbuilders.libraries;
+
+import android.content.Context;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * Created by Erick Sanchez on 28/10/2016.
+ * CEO & CTO App Builders 
+ * This class have the objective to be the central of files
+ */
+
+public class DataManager extends FileManager, Crypher, JsonManager {
+
+	/**
+    * Method to save data into a file
+    * @param folderName: Folder to take the file
+    * @prama fileName: Name of the file to append the content
+    * @param content: The content to be write o append
+    */    
+    protected void saveData( String folderName, String fileName, String content ) {
+        
+        try {
+
+            FileWriter file = this.getFileWriter(folderName, fileName);
+            file.write(content);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            Log.e("AB_DEV", "Error in writing: " + e.getLocalizedMessage());
+        }
+    }
+
+    /**
+    * Method to get saved raw data in a file
+    * @param folderName: Folder to take the file
+    * @prama fileName: Name of the file to read the content
+    */
+    @Nullable
+    protected String getData( String folderName, String fileName ) {
+        
+        try {
+
+            File f = this.getFile(folderName, fileName);
+            FileInputStream is = new FileInputStream(f);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer);
+
+        } catch (IOException e) {
+            Log.e("EB_DEV", "Error in reading: " + e.getLocalizedMessage());
+        }
+
+        return null;
+    }
+
+
+    /**
+    * Method to get data from assets folder
+    * @param fileName: Name of the fiel into the folder
+    * @param codification: Type of codification for file
+    */
+    @Nullable
+    public String getDataFromAssets( String fileName, String codification ) {
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer, codification);
+        } catch (IOException e) {
+            Log.e("DXGO", "Error in Reading: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+
+    /**
+    * Method to get data from assets folder with default codification
+    * @param fileName: Name of the fiel into the folder
+    */
+    @Nullable
+    public String getDataFromAssets( String fileName ) {
+
+        return this.getDataFromAssets(fileName, "UTF-8");
+    }
+
+    /**
+    * Method to get data from assets folder with default codification
+    * @param fileName: Name of the fiel into the folder
+    */
+    @Nullable
+    public String getDataFromAssets2( String fileName ) {
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            Writer writer = new StringWriter();
+            char[] buffer = new char[1024];
+            try {
+                Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                int n;
+                while ((n = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, n);
+                }
+                String jsonString = writer.toString();
+                return jsonString;
+            } finally {
+                is.close();
+            }
+        } catch (IOException e) {
+            Log.e("DXGO", "Error in Reading: " + e.getLocalizedMessage());
+            return null;
+        }
+    }
+
+}
--- FileManager.java
+++ FileManager.java
@@ -1,17 +1,220 @@
 package com.appbuilders.libraries;
 
+import android.content.Context;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 /**
- * Created by saer6003 on 26/01/2017.
+ * Created by Erick Sanchez on 26/01/2017.
+ * CEO & CTO App Builders 
+ * This class have the objective to be the central of files 
  */
 
 public class FileManager {
 
-    public FileManager() {
+    protected Context context;
+
+    /**
+    * Constructor
+    * @param context: The application context
+    */
+    public FileManager(Context context) {
+
+        this.context = context;
+    }
+
+
+    /**
+    * Method to create root path
+    */
+    @Nullable
+    public File makeRootPath() {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() );
+
+        if(!path.exists()){
+            path.mkdirs();
+        }
+
+        return path;
+    }
+
+    /**
+    * Method to check if the root path exists
+    */
+    @NonNull
+    public Boolean checkRootPath() {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() );
+
+        return path.exists();
+    }
+
+    /**
+    * Method to get the root path
+    */
+    @Nullable
+    public File getRootPath() {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() );
+
+        if ( path.exists() ) {
+            return path;
+        }
+
+        return null;
+    }
+
+    /**
+    * Mehtod to create a folder into the root path
+    * @param folderName: The folder's name
+    */
+    @Nullable
+    public File makeFolder(String folderName) {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() + "/" + folderName );
+
+        if ( !path.exists() ) {
+            path.mkdirs();
+        }
+
+        return path;
+    }
+
+    /**
+    * Method to check if a folder exists
+    * @param folderName: The folder's name
+    */
+    @Nullable
+    public Boolean checkFolder(String folderName) {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() + "/" + folderName );
+
+        return path.exists();
+    }
+
+    /**
+    * Method to get a folder
+    * @param folderName: The folder's name
+    */
+    @Nullable
+    public File getFolder(String folderName) {
+
+        File path = Environment.getExternalStorageDirectory();
+        path = new File( path.getPath() + "/Android/data/" + context.getPackageName() + "/" + folderName );
+
+        if ( path.exists() ) {
+            return path;
+        }
+        return null;
+    }
+
+    /**
+    * Method to create a file
+    * @param folderName: Folder to create the file
+    * @param fileName: Name of the file to be created
+    */
+    @Nullable
+    public File makeFile( String folderName, String fileName ) {
+
+        File path;
+
+        if ( folderName.compareTo("") == 0 || folderName.equalsTo("root") ) {
+            path = getRootPath(this.context);
+        } else {
+            path = this.getFolder(folderName);
+        }
+
+        return new File( path + "/" + fileName );
+    }
+
+    /**
+    * Method to check a file
+    * @param folderName: Folder to check the file
+    * @param fileNmae: Name of the fiel to be checked
+    */
+    @Nullable
+    public Boolean checkFile( String folderName, String fileName ) {
+
+        File path;
+
+        if ( folderName.compareTo("") == 0 || folderName.equalsTo("root") ) {
+            path = getRootPath(context);
+        } else {
+            path = this.getFolder(folderName);
+        }
+
+        File f = new File( path + "/" + fileName );
+        return f.exists();
+    }
+
+    /**
+    * Method to get a file
+    * @param folderName: Folder to check the file
+    * @param fileNmae: Name of the fiel to be checked
+    */
+    @Nullable
+    public File getFile( String folderName, String fileName ) {
+
+        File path;
+
+        if ( folderName.compareTo("") == 0 || folderName.equalsTo("root") ) {
+            path = getRootPath(context);
+        } else {
+            path = this.getFolder(folderName);
+        }
+
+        File file = new File( path + "/" + fileName );
+        return file;
+    }
+
+    /**
+    * Method to get a file wirter
+    * @param folderName: Folder to check the file
+    * @param fileNmae: Name of the fiel to be checked
+    */
+    @Nullable
+    public FileWriter getFileWriter( String folderName, String fileName ) {
+
+        File path;
+
+        if ( folderName.compareTo("") == 0 || folderName.equalsTo("root") ) {
+            path = getRootPath(context);
+        } else {
+            path = this.getFolder(folderName);
+        }
+
+        FileWriter file = new FileWriter( path + "/" + fileName );
+        return file;
+    }
+
+    /**
+    * Method to list external storage
+    */
+    @Nullable
+    public void listExternalStorage() {
 
         String path = Environment.getExternalStorageDirectory().getPath();
         File f = new File(path);
